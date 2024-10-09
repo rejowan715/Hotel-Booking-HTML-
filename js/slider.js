14921def7d7dbd1f0d3053ed1b420160d0a50f9a
rejowan715 <rejowan715@gmail.com>
2024-10-09 18:44:22 +0600

    html project

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  // centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
